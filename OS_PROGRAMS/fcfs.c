748221f0b0b9821b5591a97f55f241f4ac64c75e
Amarnath C <amarnathcharichilil@gmail.com>
2024-11-12 16:06:08 +0530

    Create fcfs.c

---
+++ OS_PROGRAMS/fcfs.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+    int id;
+    int arrival_time;
+    int burst_time;
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+} Process;
+
+void main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    Process processes[10];
+    for (int i = 0; i < n; i++) {
+        processes[i].id = i + 1;
+        printf("Enter the arrival time of process %d: ", i + 1);
+        scanf("%d", &processes[i].arrival_time);
+        printf("Enter the burst time of process %d: ", i + 1);
+        scanf("%d", &processes[i].burst_time);
+    }
+    int current_time = 0;
+    for (int i = 0; i < n; i++) {
+        if (current_time < processes[i].arrival_time) {
+            current_time = processes[i].arrival_time;
+        }
+        processes[i].completion_time = current_time + processes[i].burst_time;
+        processes[i].turnaround_time = processes[i].completion_time - processes[i].arrival_time;
+        processes[i].waiting_time = processes[i].turnaround_time - processes[i].burst_time;
+        current_time = processes[i].completion_time;
+    }
+    printf("Process ID\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].id, processes[i].arrival_time, processes[i].burst_time, processes[i].completion_time, processes[i].turnaround_time, processes[i].waiting_time);
+    }
+}
